2fe5a248097ad39f2475da36adb962db753249aa
VaibhavDada <VaibhavDada@Vaibhav>
2020-09-02 19:09:23 +0530

    1st commit

---
+++ src/CollectionsDemo.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class CollectionsDemo {
+public static void main(String[] args) {
+	Set<Object> s = new HashSet<>();
+	s.add("Ankur");
+	s.add(25);
+	s.add(false);
+	s.add(6.3);
+	
+	List<Object> lst = new ArrayList<>(s);
+	for(Object val:lst){
+		System.out.println(val);
+	}
+		System.out.println("Extra:"+lst.get(2).getClass());
+		System.err.println("=================================");
+		List<Object>lst1=new ArrayList<>();
+		lst1.addAll(s);
+		for ( Object val : lst1) {
+			System.out.println(val);
+		}
+	
+}
+}
