9a01e9e0b60760b12eebc7e3aa0a20c45d4db949
VaibhavDada <VaibhavDada@Vaibhav>
2020-09-05 20:16:49 +0530

    5th comit

--- src/DemoLaunch.java
+++ src/DemoLaunch.java
@@ -5,7 +5,7 @@ public static void main (String[]args){
 	
 	System.setProperty("webdriver.chrome.driver",".\\software\\chromedriver.exe");
 	ChromeDriver driver = new ChromeDriver();
-	driver.get("https://www.facebook.com/");
+	driver.navigate().to("https://www.facebook.com/");
 	driver.close();
 }
 }
--- src/deemo.java
+++ src/deemo.java
@@ -1,4 +1,22 @@
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class deemo {
+	public static void main(String[] args) throws InterruptedException  {
+		
+	System.setProperty("webdriver.chrome.driver",".\\software\\chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	
+	driver.manage().window().maximize();
+	driver.navigate().to("https://www.google.com/");
+	Thread.sleep(2000);
+	driver.navigate().back();
+	Thread.sleep(2000);
+	driver.navigate().forward();
+	driver.close();
+	driver.get("https://www.facebook.com/");
+	Thread.sleep(2000);
+	driver.navigate().refresh();
+	driver.close();
+}
 
 }
